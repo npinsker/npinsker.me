a2a8460572f5a466106c048ba066c9da089d8827
Nathan Pinsker <npinsker@gmail.com>
2022-05-04 19:58:52 -0700

    move twinleaf to its own repo

--- src/twinleaf/src/constants.js
+++
@@ -1,9 +0,0 @@
-export const CURRENT_VERSION = "0.1";
-
-export const DEFAULT_SHADER_TEXT = `precision mediump float;
-
-uniform vec3 color;
-
-void main() {
-    gl_FragColor = vec4(color, 1.0);
-}`
